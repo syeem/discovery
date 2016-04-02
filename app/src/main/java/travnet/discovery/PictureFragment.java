d8e1bf3cf6c282a54b84814fc264faccff1840cb
Hassan <hassan.ali.askari@gmail.com>
2016-04-02 22:48:34 +0800

    Added infinite scrolling list of pictures

--- app/src/main/java/travnet/discovery/PictureFragment.java
+++ app/src/main/java/travnet/discovery/PictureFragment.java
@@ -4,9 +4,35 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 /**
@@ -22,6 +48,18 @@ public class PictureFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public static final int LOADING_THRESHOLD = 5;
+    public static final int NO_OF_IMAGES = 10;
+    public static final String URL_GET_STRING = "http://54.86.18.174/api/getImages";
+
+    //View handlers
+    static ArrayList<String> imageUrls;
+    LinearLayout layout;
+    ListView listView;
+    //Universal Image Loader variables
+    ImageLoader imageLoader;
+    DisplayImageOptions options;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,13 +96,23 @@ public class PictureFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        //Initialize
+        imageUrls = new ArrayList<String>();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getContext()).build();
+        ImageLoader.getInstance().init(config);
+        imageLoader = ImageLoader.getInstance();
+        requestPictures();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_picture, container, false);
+        View view = inflater.inflate(R.layout.fragment_picture, container, false);
+        listView = (ListView) view.findViewById(R.id.list);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,4 +153,182 @@ public class PictureFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+    // Function to make http request for pictures. The received image urls are added to imageUrls.
+    // During the first call the listView is initialized
+    private void requestPictures () {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = URL_GET_STRING;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONArray arrayJson = jsonObj.getJSONArray("image");
+                            for (int i=0; i< NO_OF_IMAGES; i++) {
+                                imageUrls.add(arrayJson.getString(i));
+                            }
+                            if (imageUrls.size() == NO_OF_IMAGES)
+                                initializeListView();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getApplicationContext(), R.string.error_connect_server_failed, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    //Function to initialze the listView with  the adapter and the infinite scroll listener
+    private void initializeListView () {
+        ((ListView) listView).setAdapter(new ImageAdapter(getActivity()));
+
+        listView.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                requestPictures();
+                return false;
+            }
+        });
+
+    }
+
+
+    //Adapter to populate listView with picture cards
+    private static class ImageAdapter extends BaseAdapter {
+
+        private LayoutInflater inflater;
+        private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+        private DisplayImageOptions options;
+
+        ImageAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+
+            options = new DisplayImageOptions.Builder()
+                    .showImageOnLoading(R.mipmap.ic_loading)
+                    .cacheInMemory(true)
+                    .cacheOnDisk(true)
+                    .considerExifParams(true)
+                    .build();
+        }
+
+        @Override
+        public int getCount() {
+            return imageUrls.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            final ViewHolder holder;
+            if (convertView == null) {
+                view = inflater.inflate(R.layout.card_picture, parent, false);
+                holder = new ViewHolder();
+                holder.description = (TextView) view.findViewById(R.id.description);
+                holder.image = (ImageView) view.findViewById(R.id.image);
+                holder.likes = (TextView) view.findViewById(R.id.likes);
+                holder.activity = (TextView) view.findViewById(R.id.activity);
+                holder.location = (TextView) view.findViewById(R.id.location);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+
+            //Place Holders
+            holder.description.setText("Description");
+            holder.likes.setText("30 Likes");
+            holder.activity.setText("Some activity");
+            holder.location.setText("Some Location");
+
+            ImageLoader.getInstance().displayImage(imageUrls.get(position), holder.image, options, animateFirstListener);
+
+            return view;
+        }
+    }
+
+    static class ViewHolder {
+        TextView description;
+        ImageView image;
+        TextView likes;
+        TextView activity;
+        TextView location;
+    }
+
+    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+        //Stub
+    }
+
+
+    //Scroll listener to load more cards when scrolling nears end
+    public abstract class EndlessScrollListener implements ListView.OnScrollListener {
+        private int visibleThreshold = LOADING_THRESHOLD;
+        private int currentPage = 0;
+        private int previousTotalItemCount = 0;
+        private boolean loading = true;
+        private int startingPageIndex = 0;
+
+        public EndlessScrollListener() {
+        }
+
+        public EndlessScrollListener(int visibleThreshold) {
+            this.visibleThreshold = visibleThreshold;
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
+        {
+            // If it's still loading, we check to see if the dataset count has
+            // changed, if so we conclude it has finished loading and update the current page
+            // number and total item count.
+            if (loading && (totalItemCount > previousTotalItemCount)) {
+                loading = false;
+                previousTotalItemCount = totalItemCount;
+                currentPage++;
+            }
+
+            // If it isn't currently loading, we check to see if we have breached
+            // the visibleThreshold and need to reload more data.
+            // If we do need to reload some more data, we execute onLoadMore to fetch the data.
+            if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                loading = onLoadMore(currentPage + 1, totalItemCount);
+            }
+        }
+
+        // Defines the process for actually loading more data based on page
+        // Returns true if more data is being loaded; returns false if there is no more data to load.
+        public abstract boolean onLoadMore(int page, int totalItemsCount);
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            // Don't take any action on changed
+        }
+    }
+
+
+
+
+
+
 }
