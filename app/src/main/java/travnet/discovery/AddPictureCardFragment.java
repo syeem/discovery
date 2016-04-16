1801aa3288943580e625806fd51ebf54eb6b42a5
Hassan <hassan.ali.askari@gmail.com>
2016-04-16 23:32:27 +0800

    Added inputs for location and activity in upload picture card

--- app/src/main/java/travnet/discovery/AddPictureCardFragment.java
+++ app/src/main/java/travnet/discovery/AddPictureCardFragment.java
@@ -1,17 +1,30 @@
 package travnet.discovery;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 
 /**
@@ -27,10 +40,15 @@ public class AddPictureCardFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    EditText add_location;
+    AutoCompleteTextView activityAutoCompleteView;
+    String[] activityList = { "Sightseeing", "Surfing", "Trekking", "Yoga"};
 
     private OnFragmentInteractionListener mListener;
 
@@ -72,11 +90,20 @@ public class AddPictureCardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_picture_card, container, false);
-        ImageView previewImage = (ImageView) view.findViewById(R.id.previewImage);
 
+        ImageView previewImage = (ImageView) view.findViewById(R.id.previewImage);
         Parcelable parcelable = getArguments().getParcelable("uri");
         Uri uri = (Uri) parcelable;
         previewImage.setImageURI(uri);
+
+        add_location = (EditText) view.findViewById(R.id.add_location);
+        add_location.setOnClickListener(getLocation);
+
+        activityAutoCompleteView = (AutoCompleteTextView) view.findViewById(R.id.add_activity);
+        ArrayAdapter<String> activityAdapter = new ArrayAdapter<String>
+                (getContext(),android.R.layout.select_dialog_item, activityList);
+        activityAutoCompleteView.setAdapter(activityAdapter);
+
         return view;
     }
 
@@ -104,18 +131,44 @@ public class AddPictureCardFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+
+    EditText.OnClickListener getLocation = new EditText.OnClickListener() {
+       @Override
+        public void onClick(View v) {
+           try {
+               Intent intent =
+                       new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                               .build(getActivity());
+               startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+           } catch (GooglePlayServicesRepairableException e) {
+               Toast.makeText(getActivity().getApplicationContext(), R.string.error_get_location_failed, Toast.LENGTH_LONG).show();
+           } catch (GooglePlayServicesNotAvailableException e) {
+               Toast.makeText(getActivity().getApplicationContext(), R.string.error_get_location_failed, Toast.LENGTH_LONG).show();
+           }
+        }
+    };
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(getContext(), data);
+                add_location.setText(place.getName());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(getContext(), data);
+                // TODO: Handle the error.
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
