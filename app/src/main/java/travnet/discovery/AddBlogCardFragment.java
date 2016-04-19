4cadca660c3faebe44190f1fca2b6a3e1fafce94
Hassan <hassan.ali.askari@gmail.com>
2016-04-19 16:46:48 +0800

    Added uploading of blog card

--- app/src/main/java/travnet/discovery/AddBlogCardFragment.java
+++ app/src/main/java/travnet/discovery/AddBlogCardFragment.java
@@ -1,21 +1,41 @@
 package travnet.discovery;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.leocardz.link.preview.library.LinkPreviewCallback;
 import com.leocardz.link.preview.library.SourceContent;
 import com.leocardz.link.preview.library.TextCrawler;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -36,11 +56,17 @@ public class AddBlogCardFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    String testURL = "http://www.alexinwanderland.com/2016/03/09/antelope-canyon-lake-powell/";
-    TextView title;
-    TextView extract;
-    ImageView thumbnail;
+    String blogURL;
+    String blogTitle;
+    String blogExtract;
+    Bitmap blogThumbnail;
 
+    EditText inputBlogLink;
+    Button buttonPreviewBlog;
+    TextView previewTitle;
+    TextView previewExtract;
+    ImageView previewThumbnail;
+    Button buttonPostBlog;
 
     private OnFragmentInteractionListener mListener;
 
@@ -81,13 +107,30 @@ public class AddBlogCardFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_blog_card, container, false);
 
-        title = (TextView) view.findViewById(R.id.title);
-        extract = (TextView) view.findViewById(R.id.extract);
-        thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        inputBlogLink = (EditText) view.findViewById(R.id.blog_link);
+        buttonPreviewBlog = (Button) view.findViewById(R.id.button_preview_blog);
+        previewTitle = (TextView) view.findViewById(R.id.title);
+        previewExtract = (TextView) view.findViewById(R.id.extract);
+        previewThumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        buttonPostBlog = (Button) view.findViewById(R.id.button_post_blog);
+
+        buttonPreviewBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogURL = inputBlogLink.getText().toString();
+                TextCrawler textCrawler = new TextCrawler();
+                textCrawler.makePreview(linkPreviewCallback, blogURL);
+            }
+        });
+
+        buttonPostBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postBlog();
+            }
+        });
 
 
-        TextCrawler textCrawler = new TextCrawler();
-        textCrawler.makePreview(linkPreviewCallback, testURL);
 
         return view;
     }
@@ -140,12 +183,69 @@ public class AddBlogCardFragment extends Fragment {
 
         @Override
         public void onPos(SourceContent sourceContent, boolean b) {
-            title.setText(sourceContent.getTitle());
-            extract.setText(sourceContent.getDescription());
-            String thumbnailUrl = sourceContent.getImages().get(0);
-            ImageLoader.getInstance().displayImage(thumbnailUrl, thumbnail);
+            blogTitle = sourceContent.getTitle();
+            blogExtract = sourceContent.getDescription();
 
+            previewTitle.setText(blogTitle);
+            previewExtract.setText(blogExtract);
+            String thumbnailUrl = sourceContent.getImages().get(0);
+            ImageLoader imageLoader = ImageLoader.getInstance();
+            imageLoader.loadImage(thumbnailUrl, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    blogThumbnail = loadedImage;
+                    previewThumbnail.setImageBitmap(loadedImage);
+                }
+            });
+            //ImageLoader.getInstance().displayImage(thumbnailUrl, previewThumbnail);
         }
     };
+
+
+    public void postBlog() {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = "http://54.86.18.174/api/postBlog";
+
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getApplicationContext(), R.string.error_connect_server_failed, Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("card-type", "blog");
+                params.put("url", blogURL);
+                params.put("title", blogTitle);
+                params.put("extract", blogExtract);
+                String image = getStringImage(blogThumbnail);
+                params.put("thumbnail", image);
+
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public String getStringImage(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+
 
 }
