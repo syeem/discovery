cef34603a6efc3eb8f4983fdee0613b7c8e5e97b
Hassan <hassan.ali.askari@gmail.com>
2016-05-27 15:59:06 +0800

    Implemented recycler view in bucket list activity

---
+++ app/src/main/java/travnet/discovery/DataBucketListCard.java
@@ -0,0 +1,29 @@
+package travnet.discovery;
+
+/**
+ * Created by root on 5/27/16.
+ */
+
+public class DataBucketListCard {
+
+    String imageLink;
+    String location;
+    String activity;
+
+    public DataBucketListCard() {
+    }
+
+    public DataBucketListCard(String imageLink, String location, String activity) {
+        this.imageLink = imageLink;
+        this.location = location;
+        this.activity = activity;
+
+    }
+
+
+}
+
+
+
+
+
